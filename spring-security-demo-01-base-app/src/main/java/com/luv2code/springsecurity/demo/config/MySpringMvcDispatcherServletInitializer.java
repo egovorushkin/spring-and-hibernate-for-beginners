ed18229706234f4248a1fe9b1be78952ed079eac
Evgenii Govorushkin <e.govorushkin@gmail.com>
2019-11-11 23:56:34 +0300

    delete some comments

---
+++ spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] { "/" };
+	}
+
+}
