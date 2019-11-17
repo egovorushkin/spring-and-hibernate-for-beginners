a6514298b5185090ebdc740964dad9d50117e5d9
Evgenii Govorushkin <e.govorushkin@gmail.com>
2019-11-17 23:06:19 +0300

    jackson-databinding-json-demo

---
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,40 @@
+package com.luv2code.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// read JSON file and map/convert to Java POJO:
+			// data/simple-lite.json
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			// print First name and Last name
+			System.out.println("First name: " + theStudent.getFirstName());
+			System.out.println("Last name: " + theStudent.getLastName());
+
+			// print our address: street and city
+			Address tempAddress = theStudent.getAddress();
+
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+
+			// print out languages
+			System.out.print("Languages:");
+			for (String tempLang : theStudent.getLanguages()) {
+				System.out.print(" " + tempLang + ",");
+			}
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
