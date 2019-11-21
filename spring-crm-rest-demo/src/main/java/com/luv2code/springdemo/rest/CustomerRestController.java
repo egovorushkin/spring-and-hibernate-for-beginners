af2244713d548696df42dd6121301cf03597068b
Evgenii <johnnywas87@gmail.com>
2019-11-21 06:39:52 +0300

    added Customer Exception Handler and Add Customer features

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -2,9 +2,13 @@ package com.luv2code.springdemo.rest;
 
 import java.util.List;
 
+import javax.persistence.PostUpdate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +35,24 @@ public class CustomerRestController {
 	public Customer getCustomer(@PathVariable int customerId) {
 
 		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		if(theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found " + customerId);
+		}
 
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
 		return theCustomer;
 	}
 }
