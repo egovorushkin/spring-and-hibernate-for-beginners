05559d871e7fc8524f6c1257d9e95e9f6e9a08d5
Evgenii <johnnywas87@gmail.com>
2019-11-21 06:53:51 +0300

    added Update Customer feature

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,24 +36,33 @@ public class CustomerRestController {
 	public Customer getCustomer(@PathVariable int customerId) {
 
 		Customer theCustomer = customerService.getCustomer(customerId);
-		
-		if(theCustomer == null) {
+
+		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found " + customerId);
 		}
 
 		return theCustomer;
 	}
-	
+
 	// add mapping for POST /customers - add new customer
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
-		
+
 		// also just in case the pass an id in JSON ... set id to 0
 		// this is force a save of new item ... instead of update
 		theCustomer.setId(0);
-		
+
 		customerService.saveCustomer(theCustomer);
-		
+
+		return theCustomer;
+	}
+
+	// add mapping for PUT /customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+
+		customerService.saveCustomer(theCustomer);
+
 		return theCustomer;
 	}
 }
