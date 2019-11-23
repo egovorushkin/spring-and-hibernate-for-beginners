d5d231ce2a6b832259cbd454e145ae0921e818b8
Evgenii <johnnywas87@gmail.com>
2019-11-24 00:07:31 +0300

    04-command-line-demo

---
+++ 04-command-line-demo/src/main/java/com/luv2code/springboot/demo/mycoolappspringboot/rest/FunRestController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.demo.mycoolappspringboot.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	// expose "/" that returns "Hello World!"
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hello world! Time on server is " + LocalDateTime.now();
+	}
+	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		
+		return "Run a Hard 5k!";
+	}
+	
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		
+		return "Today is your lucky day!";
+	}
+
+}
---
+++ 04-command-line-demo/src/test/java/com/luv2code/springboot/demo/mycoolappspringboot/MycoolappSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
