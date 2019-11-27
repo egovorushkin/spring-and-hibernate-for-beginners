5468ab98370c61b0d3cf04da92eae411b745eff6
Evgenii <johnnywas87@gmail.com>
2019-11-27 06:45:07 +0300

    24-spring-data-rest-cruddemo

--- 24-spring-data-rest-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
